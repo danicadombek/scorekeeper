f651c7ec748a5f3c3a85fcf06fc1712ca4c89c5a
Danica Dombek <danica.dombek@gmx.de>
2021-05-18 14:24:45 +0200

    Add functionality in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Player from './Player'
+import Button from './Button'
+import PlayerForm from './PlayerForm'
+import { useState } from 'react'
+
+import './App.css'
 
 function App() {
+  const [players, setPlayers] = useState([
+    { name: 'John Doe', score: 20 },
+    { name: 'jane Doe', score: 40 },
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      {players.map((player, index) => (
+        <Player
+          onMinus={() => handleMinus(index)}
+          onPlus={() => handlePlus(index)}
+          key={player.name}
+          name={player.name}
+          score={player.score}
+        />
+      ))}
 
-export default App;
+      <Button onClick={resetScores}>Reset scores</Button>
+      <Button onClick={resetAll}>Reset All</Button>
+      <PlayerForm onSubmit={createPlayer} />
+    </div>
+  )
+
+  function resetScores() {
+    setPlayers(players.map(player => ({ ...player, score: 0 })))
+  }
+
+  function resetAll() {
+    setPlayers([])
+  }
+
+  function handleMinus(index) {
+    const playerToUpdate = players[index]
+    setPlayers([
+      ...players.slice(0, index),
+      { ...playerToUpdate, score: playerToUpdate.score - 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
+  function handlePlus(index) {
+    const playerToUpdate = players[index]
+    setPlayers([
+      ...players.slice(0, index),
+      { ...playerToUpdate, score: playerToUpdate.score + 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
+  function createPlayer(name) {
+    setPlayers([...players, { name, score: 0 }])
+  }
+}
+export default App
